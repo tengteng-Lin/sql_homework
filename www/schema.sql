cc6e16d7c7d6e8018490a257737c42495b36c2f3
ltt <nanyi_Gu@163.com>
2018-08-08 21:50:41 +0800

    部部分

--- www/schema.sql
+++ www/schema.sql
@@ -7,5 +7,20 @@ use awesome;
 grant select ,insert ,update,delete on awesome * to 'www-data'@'localhost' identified by 'www-data';
 
 create table users(
-    'UserID'
-)
+    'UserID' VARCHAR (18) NOT NULL ,
+    'User' VARCHAR (50) NOT NULL ,
+    'Sex' VARCHAR (50),
+    'Phone'INT NOT NULL
+)engine=innodb DEFAULT charset=utf8
+
+CREATE TABLE buese(
+    'BusID'VARCHAR (10) NOT NULL ,
+    'BusFrom'VARCHAR (50) NOT NULL ,
+    'BusTo' VARCHAR (50) not NULL ,
+    'BusDate'FLOAT NOT NULL
+    'BusEnd'FLOAT not NULL
+    'TicketNum'int NOT NULL
+    'Price' INT NOT NULL
+)engine=innodb DEFAULT charset=utf8
+
+CREATE TABLE
