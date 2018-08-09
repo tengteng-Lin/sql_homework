62e2c1b3d4212a89a0e23719cd1510d6469784a6
ltt <nanyi_Gu@163.com>
2018-08-09 21:33:14 +0800

    sql脚本缺主键等约束

--- www/schema.sql
+++ www/schema.sql
@@ -4,23 +4,31 @@ create database awesome;
 
 use awesome;
 
-grant select ,insert ,update,delete on awesome * to 'www-data'@'localhost' identified by 'www-data';
+grant select, insert, update, delete on awesome.* to 'www-data'@'localhost' identified by 'www-data';
 
 create table users(
-    'UserID' VARCHAR (18) NOT NULL ,
-    'User' VARCHAR (50) NOT NULL ,
-    'Sex' VARCHAR (50),
-    'Phone'INT NOT NULL
-)engine=innodb DEFAULT charset=utf8
+    `UserID` VARCHAR (18) NOT NULL ,
+    `User` VARCHAR (50) NOT NULL ,
+    `Sex` VARCHAR (50),
+    `Phone` INT NOT NULL
+)engine=innodb DEFAULT charset=utf8;
 
-CREATE TABLE buese(
-    'BusID'VARCHAR (10) NOT NULL ,
-    'BusFrom'VARCHAR (50) NOT NULL ,
-    'BusTo' VARCHAR (50) not NULL ,
-    'BusDate'FLOAT NOT NULL
-    'BusEnd'FLOAT not NULL
-    'TicketNum'int NOT NULL
-    'Price' INT NOT NULL
-)engine=innodb DEFAULT charset=utf8
+CREATE TABLE buses(
+    `BusID` VARCHAR (10) NOT NULL ,
+    `BusFrom` VARCHAR (50) NOT NULL ,
+    `BusTo` VARCHAR (50) not NULL ,
+    `BusDate` FLOAT NOT NULL,
+    `BusEnd` FLOAT not NULL,
+    `TicketNum` int NOT NULL,
+    `Price` INT NOT NULL
+)engine=innodb DEFAULT charset=utf8;
 
-CREATE TABLE
+CREATE TABLE orders(
+    `OrderID` VARCHAR (10) NOT NULL ,
+    `UserID` VARCHAR (18) NOT NULL ,
+    `BusID` VARCHAR (10) NOT NULL ,
+    `BusDate` FLOAT NOT NULL ,
+    `OrderDate` FLOAT NOT NULL ,
+    `OrderNum` FLOAT NOT NULL ,
+    `Total` FLOAT NOT NULL
+)engine=innodb DEFAULT charset=utf8
