b5584a5b046209852810bdad3d476abe70c98abe
ltt <nanyi_Gu@163.com>
2018-08-14 21:18:42 +0800

    finished coroweb.py

--- www/coroweb.py
+++ www/coroweb.py
@@ -148,4 +148,33 @@ class RequestHandler(object):
 
 
 def add_static(app):
-    path = os.path.join()
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'static')
+    app.router.add_static('/static/',path)
+    logging.info('add static %s => %s' % ('/static/',path))
+
+def add_route(app,fn):
+    method = getattr(fn,'__method__',None)
+    path = getattr(fn,'route',None)
+    if path is None or method is None:
+        raise ValueError('@get or @post not defined in %s.' % str(fn))
+    if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
+        fn = asyncio.coroutine(fn)
+    logging.info('add route %s %s => %s(%s)' % (method,path,fn.__name__,','.join(inspect.signature(fn).parameters.keys())))
+    app.router.add_route(method,path,RequestHandler(app,fn))
+
+def add_routes(app,moudle_name):
+    n = moudle_name.rfind('.')
+    if n == (-1):
+        mod = __import__(moudle_name,globals(),locals())
+    else:
+        name = moudle_name[n+1:]
+        mod = getattr(__import__(moudle_name[:n],globals(),locals(),[name]),name)
+    for attr in dir(mod):
+        if attr.startswith('_'):
+            continue
+        fn  = getattr(mod,attr)
+        if callable(fn):
+            method = getattr(fn,'__method__',None)
+            path = getattr(fn,'__route__',None)
+            if method and path:
+                add_route(app,fn)
