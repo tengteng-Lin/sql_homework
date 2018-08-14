bb286c6bd230e74c5fd9d360e1b76142ec3820c4
ltt <nanyi_Gu@163.com>
2018-08-14 21:07:07 +0800

    ..

--- www/coroweb.py
+++ www/coroweb.py
@@ -40,6 +40,11 @@ def post(path):
     return decorator
 
 def get__required_kw_args(fn):
+    '''
+    
+    :param fn: 
+    :return: 
+    '''
     args = []
     params = inspect.signature(fn).parameters
     for name,param in params.items():
@@ -54,3 +59,93 @@ def get_named_kw_args(fn):
         if param.kind == inspect.Parameter.KEYWORD_ONLY:
             args.append(name)
     return tuple(args)
+
+def has_named_kw_args(fn):
+    params = inspect.signature(fn).parameters
+    for name,param in params.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY:
+            return True
+
+def has_var_kw_arg(fn):
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.VAR_KEYWORD:
+            return True
+
+def has_request_arg(fn):
+    sig = inspect.signature(fn)
+    params = sig.parameters
+    found = False
+    for name,param in params.items():
+        if name == 'request':
+            found = True
+            continue
+        if found and (param.kind != inspect.Parameter.VAR_POSITIONAL and param.kind != inspect.Parameter.KEYWORD_ONLY and param.kind != inspect.Parameter.VAR_KEYWORD):
+            raise ValueError('request parameter must be the last named parameter in function:%s%s' % (fn.__name__,str(sig)))
+    return found
+
+class RequestHandler(object):
+    def __init__(self,app,fn):
+        self._app = app
+        self._func = fn
+        self._has_request_arg = has_request_arg(fn)
+        self._has_var_kw_arg = has_var_kw_arg(fn)
+        self._has_named_kw_args = has_named_kw_args(fn)
+        self._named_kw_args = get_named_kw_args(fn)
+        self._required_kw_args = get__required_kw_args(fn)
+
+    async def __call__(self, request):
+        kw = None
+        if self._has_var_kw_arg or self._has_named_kw_args or self._required_kw_args:
+            if request.method == 'POST':
+                if not request.content_type:
+                    return web.HTTPBadRequest('Missing Content-type')
+                ct = request.content_type.lower()
+                if ct.startwith('application/json'):
+                    params = await request.json()
+                    if not isinstance(params,dict):
+                        return web.HTTPBadRequest('JSON body must be object')
+                    kw = params
+                elif ct.startwith('application/x-www-form-urlencoded') or ct.startwith('multipart/form-data'):
+                    params = await request.post()
+                    kw = dict(**params)
+                else:
+                    return web.HTTPBadRequest('Unsupported Content-Type:%s' % request.content_type)
+            if request.method == 'GET':
+                qs = request.query_string
+                if qs:
+                    kw = dict()
+                    for k,v in parse.parse_qs(qs,True).items():
+                        kw[k] = v[0]
+        if kw is None:
+            kw = dict(**request.match_info)
+        else:
+            if not self._has_var_kw_arg and self._named_kw_args:
+                #remove all unamed kw
+                copy = dict()
+                for name in self._named_kw_args:
+                    if name in kw:
+                        copy[name] = kw[name]
+                kw = copy
+            #check named arg:
+            for k,v in request.match_info.items():
+                if k in kw:
+                    logging.warning('Duplicate arg name in named arg and kw args:%s' % k)
+                kw[k] = v
+        if self._has_request_arg:
+            kw['request'] = request
+        #check required kw
+        if self._required_kw_args:
+            for name in self._required_kw_args:
+                if not name in kw:
+                    return web.HTTPBadRequest('Missing argument:%s' % name)
+        logging.info('call with args:%s' % str(kw))
+        try:
+            r = await self._func(**kw)
+            return r
+        except APIError as e:
+            return dict(error=e.error,data=e.data,message=e.message)
+
+
+def add_static(app):
+    path = os.path.join()
