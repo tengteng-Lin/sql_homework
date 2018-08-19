096da95e157e2cdd9b571310eb9625491a17c873
ltt <nanyi_Gu@163.com>
2018-08-19 21:29:13 +0800

    啊。。雨下好大，完全不想学习，哭哭。。。

--- www/app.py
+++ www/app.py
@@ -10,8 +10,22 @@ def init_jinja2(app,**kw):
     logging.info('init jinja2')
     options = dict(
         autoescape = kw.get('autoescape',True),
-        block_start_string = kw.get('block_start_string','{%')
+        block_start_string = kw.get('block_start_string','{%'),
+        block_end_string = kw.get('block_end_string','%}'),
+        variable_start_string = kw.get('variable_start_string','{{'),
+        variable_end_string = kw.get('variable_end_string','}}'),
+        auto_reload = kw.get('auto_reload',True)
     )
+    path = kw.get('path',None)
+    if path is None:
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'templates')
+    logging.info('set jinja2 template path:%s ' % path)
+    env = Environment(loader=FileSystemLoader(path),**options)
+    filters = kw.get('filters',None)
+    if filters is not None:
+        for name,f in filters.items():
+            env.filters[name] = f
+    app['__templating__'] = env
 
 def index(request):  #requeset包含了浏览器发送过来的http协议的信息，一般不用自己构造
     return web.Response(body=b'<h1>awesome</h1>')   #构造一个http相应  类声明 class aiohttp.web.Response(*, status=200, headers=None, content_type=None, body=None, text=None)
